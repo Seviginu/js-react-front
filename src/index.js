23165391dc92c34eb70d02103ad8ffe3b8ff9569
seviginu <1500konstantin@gmail.com>
2023-12-19 01:02:42 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import LoginForm from './components/LoginForm';
+import StartPage from './pages/StartPage';
+import MainPage from './pages/MainPage';
+import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="main" element={<MainPage />} />
+      <Route path="" element={<StartPage />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <RouterProvider router={router} />
+    
   </React.StrictMode>
 );
 
